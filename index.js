1660d4bcde58c1684830e4efdf2ef74248e750da
Rosângela Nery <rosangela2nery@gmail.com>
2022-09-04 18:35:38 -0300

    Usando a lib dotenv

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+let db;
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db('batepapouol');
+});
 
 const participantes = [];
 const mensagens = [];
 
 app.post('/participants', (req, res) => {
     const { name } = req.body;
-    participantes.push({ name });
 
     if(!name) {
         res.sendStatus(422);
         return;
     }
 
+    db.collection('participantes').insertOne({ name });
+
     res.send(201);
 });
 
 app.get('/participants', (req, res) => {
 
-    console.log(participantes)
-    res.send(participantes);
+    db.collection('participantes').find().toArray().then(data => {
+        res.send(data);
+    });
 });
 
 app.post('/messages', (req, res) => {
     const { to, text, type } = req.body;
-    mensagens.push({ to, text, type });
 
     if(!to || !text || !type) {
         res.sendStatus(422);
         return;
     }
+
+    db.collection('mensagens').insertOne({ to, text, type });
 
     res.send(201);
 });
@@ -41,8 +55,10 @@ app.post('/messages', (req, res) => {
 app.get('/messages', (req, res) => {
     const limit = mensagens.slice(-100);
 
-    console.log(limit)
-    res.send(limit);
+    db.collection('mensagens').find().toArray().then(data => {
+        res.send(data);
+        res.send(limit);
+    });
 });
 
 app.listen(5000, () => console.log('Listening on port 5000'));
