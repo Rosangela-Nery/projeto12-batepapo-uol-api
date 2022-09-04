a5787b979777dbeb6bc58a5982467c2a18838a1b
Rosângela Nery <rosangela2nery@gmail.com>
2022-09-04 10:41:00 -0300

    Listening on port 5000

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const participantes = [];
+const mensagens = [];
+
+app.post('/participants', (req, res) => {
+    const { name } = req.body;
+    participantes.push({ name });
+
+    console.log(participantes);
+
+    res.send('status 422');
+});
 
 app.listen(5000, () => console.log('Listening on port 5000'));
