dff767999fb622d76c3614067ea68b9ab7361c9b
Rosângela Nery <rosangela2nery@gmail.com>
2022-09-04 22:31:17 -0300

    Adicionando funcionalidades async

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,16 +10,15 @@ app.use(cors());
 app.use(express.json());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+console.log(process.env.MONGO_URI)
+
 let db;
 
 mongoClient.connect().then(() => {
     db = mongoClient.db('batepapouol');
 });
 
-const participantes = [];
-const mensagens = [];
-
-app.post('/participants', (req, res) => {
+app.post('/participants', async (req, res) => {
     const { name } = req.body;
 
     if(!name) {
@@ -27,19 +26,25 @@ app.post('/participants', (req, res) => {
         return;
     }
 
-    db.collection('participantes').insertOne({ name });
-
-    res.send(201);
+    try {
+        const response = await db.collection('participantes').insertOne({ name })
+        res.send(response)
+    } catch (error) {
+        res.sendStatus(422);
+    }
 });
 
-app.get('/participants', (req, res) => {
+app.get('/participants', async (req, res) => {
 
-    db.collection('participantes').find().toArray().then(data => {
-        res.send(data);
-    });
+    try {
+        const response = await db.collection('participantes').find().toArray()
+        res.send(response);
+    } catch (error) {
+        res.send(201)
+    }
 });
 
-app.post('/messages', (req, res) => {
+app.post('/messages', async (req, res) => {
     const { to, text, type } = req.body;
 
     if(!to || !text || !type) {
@@ -47,18 +52,42 @@ app.post('/messages', (req, res) => {
         return;
     }
 
-    db.collection('mensagens').insertOne({ to, text, type });
-
-    res.send(201);
+    try {
+        const response = await db.collection('mensagens').insertOne({ to, text, type })
+        res.send(response)
+    } catch (error) {
+        res.send(201)
+    }
 });
 
-app.get('/messages', (req, res) => {
+app.get('/messages', async (req, res) => {
     const limit = mensagens.slice(-100);
 
-    db.collection('mensagens').find().toArray().then(data => {
-        res.send(data);
-        res.send(limit);
-    });
+    try {
+        const response = await db.collection('mensagens').find().toArray()
+        res.send(response);
+    } catch (error) {
+        res.send(201)
+    }
+});
+
+app.post('/status', async (req, res) => {
+    const {user: name} = req.headers;
+
+    res.send(200)
+});
+
+
+app.delete('/messages/:ID_DA_MENSAGEM', async (req, res) => {
+    const { ID_DA_MENSAGEM } = req.params;
+
+    try {
+        const response = await db.collection('mensagens').deleteOne({_id: ObjectId(ID_DA_MENSAGEM)});
+
+        res.send(response);
+    } catch (error) {
+        res.status(401);
+    }
 });
 
 app.listen(5000, () => console.log('Listening on port 5000'));
